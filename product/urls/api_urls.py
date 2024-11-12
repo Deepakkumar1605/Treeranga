59a69dcff1a86c29f845f29972ea4be5dc19cefa
Deepak Choudhury <dpk.choudhury.32@gmail.com>
2024-11-12 16:57:06 +0530

    reviews_DKC

--- product/urls/api_urls.py
+++ product/urls/api_urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('api/new-products/', api_product_views.AllNewProductsAPIView.as_view(), name='all-new-products-api'),
     path('api/products/<str:category_name>/', api_product_views.ShowProductsAPIView.as_view(), name='show_products_api'),
     path('api/search-product-names/', api_product_views.SearchProductNamesAPIView.as_view(), name='search-product-names'),
-    path('api/search-items/', api_product_views.SearchItemsAPIView.as_view(), name='search-items'),   
+    path('api/search-items/', api_product_views.SearchItemsAPIView.as_view(), name='search-items'), 
+    path('api/products/<int:p_id>/review/', api_product_views.ProductReviewAPIView.as_view(), name='product_review_api'),
+ 
 ]
